65e061fa3035f833f39eb94ab11c63644358d255
Федор Юрецкий <fedjaz5@gmail.com>
2020-06-19 19:23:53 +0300

    fixed mod

--- Algorithms/Mod.py
+++ Algorithms/Mod.py
@@ -4,8 +4,10 @@ a = int(input())
 print('b = ')
 b = int(input())
 if b == 0:
-    print('b не может принимать отрицательное значение')
+    print('b не может принимать нулевое значение')
     exit()
-q = int(a / b)
-r = a - b * q
+r = a % b
+if r < 0:
+    r += abs(b)
+q = (a - r) / b
 print('%d = %d * %d + %d' % (a, b, q, r))
