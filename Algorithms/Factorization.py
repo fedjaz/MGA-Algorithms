e0bb2951efe7b9978f1d9435908dc6c230bca628
Федор Юрецкий <fedjaz5@gmail.com>
2020-06-19 13:08:01 +0300

    fixed factorization

--- Algorithms/Factorization.py
+++ Algorithms/Factorization.py
@@ -1,10 +1,13 @@
 def get_dividers(a):
     a = abs(a)
-    dividers = []
+    dividers = {}
     divider = 2
     while a != 1:
         while a % divider == 0:
-            dividers.append(divider)
+            if divider in dividers:
+                dividers[divider] += 1
+            else:
+                dividers[divider] = 1
             a //= divider
         divider += 1
     return dividers
@@ -13,4 +16,6 @@ def get_dividers(a):
 print('Разложение числа а на простые множители:')
 print('a = ')
 a = int(input())
-print(get_dividers(a))
+dividers = get_dividers(a)
+s = [(str(key) if value == 1 else '%d^%d' % (key, value)) for (key, value) in dividers.items()]
+print(' * '.join(s))
